f9ee907099057909c1f9ad96f432f13e53667246
Imangel <renaatopower@gmail.com>
2024-06-12 16:07:25 -0600

    Primer commit - Inicializa el proyecto

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import {
+  blackImg,
+  blueImg,
+  highlightFirstVideo,
+  highlightFourthVideo,
+  highlightSecondVideo,
+  highlightThirdVideo,
+  whiteImg,
+  yellowImg,
+} from "../utils";
+
+export const navLists = ["Store", "Mac", "iPhone", "Support"];
+
+export const hightlightsSlides = [
+  {
+    id: 1,
+    textLists: ["El chip A17 Pro.", "llego a revolucionar todo."],
+    video: highlightFirstVideo,
+    videoDuration: 4,
+  },
+  {
+    id: 2,
+    textLists: ["Titanio.", "Tan resistente y ligero. Tan Pro."],
+    video: highlightSecondVideo,
+    videoDuration: 5,
+  },
+  {
+    id: 3,
+    textLists: [
+      "El iPhone 15 Pro Max tiene el",
+      "zoom óptico de mayor alcance en",
+      "un iPhone. Por lejos.",
+    ],
+    video: highlightThirdVideo,
+    videoDuration: 2,
+  },
+  {
+    id: 4,
+    textLists: ["Nuevo Botón de Acción", "Elige su misión."],
+    video: highlightFourthVideo,
+    videoDuration: 3.63,
+  },
+];
+
+export const models = [
+  {
+    id: 1,
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
+    img: yellowImg,
+  },
+  {
+    id: 2,
+    title: "iPhone 15 Pro in Blue Titanium",
+    color: ["#53596E", "#6395ff", "#21242e"],
+    img: blueImg,
+  },
+  {
+    id: 3,
+    title: "iPhone 15 Pro in White Titanium",
+    color: ["#C9C8C2", "#ffffff", "#C9C8C2"],
+    img: whiteImg,
+  },
+  {
+    id: 4,
+    title: "iPhone 15 Pro in Black Titanium",
+    color: ["#454749", "#3b3b3b", "#181819"],
+    img: blackImg,
+  },
+];
+
+export const sizes = [
+  { label: '6.1"', value: "small" },
+  { label: '6.7"', value: "large" },
+];
+
+export const footerLinks = [
+  "Privacy Policy",
+  "Terms of Use",
+  "Sales Policy",
+  "Legal",
+  "Site Map",
+];
